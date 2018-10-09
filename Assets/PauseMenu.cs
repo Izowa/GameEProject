a1a6cd771970091b6c05e3ce737bb305f2637d5c
Billy Zejnoski <bz555@uowmail.edu.au>
2018-10-09 14:48:08 +1100

    PauseMenu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.JoystickButton9) || Input.GetKeyDown(KeyCode.P))
+            if (gamePaused)
+                resume();
+            else
+                pause();
+
+	}
+
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        gamePaused = false;
+    }
+
+     void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("menu");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+
+    }
+}
